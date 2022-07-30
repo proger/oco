1a400ce25175d6cc2ad429371e337ab8b1842aa8
Volodymyr Kyrylov <vol@wilab.org.ua>
2022-07-30 19:17:57 +0300

    to begin: python3 -m oco

---
+++ oco/__main__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+import uvicorn
+
+uvicorn.run("oco.app:app", host="0.0.0.0", log_level="info", reload=True, reload_dirs=[Path(__file__).parent])
--- oco/app.py
+++ oco/app.py
@@ -13,8 +13,3 @@ import oco.wavesurfer
 app.include_router(oco.wavesurfer.router)
 
 app.mount("/static", StaticFiles(directory=Path(__file__).parent / "static"), name="static")
-
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run("oco.app:app", host="0.0.0.0", log_level="info", reload=True, reload_dirs=[Path(__file__).parent])
