6c7466a9f5769e63de7ace6eb0aac1af0fb2a603
Volodymyr Kyrylov <vol@wilab.org.ua>
2022-07-31 17:33:07 +0300

    mypy for __main__

--- oco/__main__.py
+++ oco/__main__.py
@@ -1,4 +1,5 @@
 from pathlib import Path
-import uvicorn
+import uvicorn  # type: ignore
 
-uvicorn.run("oco.app:app", host="0.0.0.0", log_level="info", reload=True, reload_dirs=[Path(__file__).parent])
+uvicorn.run("oco.app:app", host="0.0.0.0", log_level="info",
+            reload=True, reload_dirs=[Path(__file__).parent])
