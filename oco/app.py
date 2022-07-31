183f5b7e2c526ed8e2197a56ee77b48a0ae22165
Volodymyr Kyrylov <vol@wilab.org.ua>
2022-07-31 17:33:00 +0300

    flake8 app.py

--- oco/app.py
+++ oco/app.py
@@ -2,14 +2,15 @@ from pathlib import Path
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+import oco.index
+import oco.wavesurfer
+
 app = FastAPI(
     title="Use Oco to peek at your files",
 )
 
-import oco.index
 app.include_router(oco.index.router)
-
-import oco.wavesurfer
 app.include_router(oco.wavesurfer.router)
-
-app.mount("/static", StaticFiles(directory=Path(__file__).parent / "static"), name="static")
+app.mount("/static",
+          StaticFiles(directory=Path(__file__).parent / "static"),
+          name="static")
